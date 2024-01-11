319df95f5bfef5c74f4aac5923d24fc55d76262d
cozyin <mitchvandercline@gmail.com>
2024-01-11 14:07:48 +0100

    added styling for index

--- index.php
+++ index.php
@@ -16,11 +16,13 @@ $DBHandler->CloseConn();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="Prefabs/defaultStyle.css">
+    <link rel="stylesheet" href="Styles/Index.css">      
     <title><?php echo $PageInfo["Title"]?></title>
 </head>
 <body>
 <?php include('Prefabs/header.html') ?>
-
+<h1 class="infotextheader">Over ons</h1>
+<p class="infotextbody">Partners in Grime is een schoonmaakbedrijf met al 20 jaar ervaring in het helpen schoonhouden van jou huis.<br>Het beste schoonmaakbedrijf binnen Europa</p>
 <?php include('Prefabs/footer.html') ?>
 </body>
 </html>
