5ed856cc2b6c5d05b2c7b6a8f316fd3d98cacae0
cozyin <mitchvandercline@gmail.com>
2024-01-18 11:14:39 +0100

    Add text & style

--- index.php
+++ index.php
@@ -22,9 +22,11 @@ $DBHandler->CloseConn();
 <body>
 <?php include('Prefabs/header.html') ?>
 <h1 class="infotextheader">About us</h1>
+<p class="infotextbody">Don't know where to start searching for top notch cleaning products? You've come to the right place!</p>
 <p class="infotextbody">Partners in Grime is a cleaning company with well over 20 years of experience in keeping your house clean and tidy.</p>
 <p class="infotextbody">The best cleaning company in all of Europe!</p>
-<p class="infotextbody">placeholder</p>
+<p class="infotextbody">Need tips on how to keep your house clean? Visit our Tips and Tricks page to view the possibilities.</p>
+<p class="infotextbody">Got a question? You can always reach out to us at the bottom of the page.</p>
 <div class="infotextbuttoncontainer"><a href="products.php" class="infotextbodybutton">Take me to the shop!</a></div>
 <?php include('Prefabs/footer.html') ?>
 </body>
